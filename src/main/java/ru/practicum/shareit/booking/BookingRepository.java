5657d8e616f2e487081afb09b6e81a6cfc64043a
Dmitry Maksimov <dimax40rus@yandex.ru>
2023-06-13 17:05:05 +0300

    feat: реализован функционал 15 спринта

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
@@ -87,60 +85,3 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     Page<Booking> findAllItemsWaitingBookings(Long userId, Pageable pageable);
 }
 
-    /*@Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in :userItems " +
-            "order by b.start desc")
-    List<Booking> findAllItemsBookings(List userItems);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in ?1 " +
-            "and ?2 between b.start  and b.end " +
-            "order by b.start desc")
-    List<Booking> findAllItemsCurrentBookings(List userItems, LocalDateTime dateTime);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in ?1 " +
-            "and b.end < ?2 " +
-            "order by b.start desc")
-    List<Booking> findAllItemsPastBookings(List userItems, LocalDateTime dateTime);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in ?1 " +
-            "and b.start > ?2 " +
-            "order by b.start desc")
-    List<Booking> findAllItemsFutureBookings(List userItems, LocalDateTime dateTime);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in ?1 " +
-            "and b.status = 'REJECTED' " +
-            "order by b.start desc")
-    List<Booking> findAllItemsRejectedBookings(List userItems);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id in ?1 " +
-            "and b.status = 'WAITING' " +
-            "order by b.start desc")
-    List<Booking> findAllItemsWaitingBookings(List userItems);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "where i.id = ?1 and " +
-            "b.status = 'APPROVED' " +
-            "order by b.start desc")
-    List<Booking> findAllBookingsByItemId(long itemId);
-
-    @Query("select b from Booking as b " +
-            "join b.item as i " +
-            "join b.booker as u " +
-            "where i.id = ?1 " +
-            "and u.id = ?2 " +
-            "and b.end < ?3 ")
-    List<Booking> findAllByBookerIdAndItemIdAndEndBefore(long itemId, long bookerId, LocalDateTime end);
-
-}
