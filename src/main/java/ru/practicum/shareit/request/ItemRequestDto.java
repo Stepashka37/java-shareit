eb717eb1b722fe0c2ab0ed0eeffd0d5db9a8b039
Dmitry Maksimov <dimax40rus@yandex.ru>
2023-06-15 16:55:08 +0300

    fix: added tests

--- src/main/java/ru/practicum/shareit/request/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import ru.practicum.shareit.item.ItemDto;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -16,6 +17,7 @@ public class ItemRequestDto {
 
     private long id;
 
+    @NotBlank
     private String description;
 
     private LocalDateTime created;
